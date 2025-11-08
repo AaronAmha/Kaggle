0dd836a7d6c8df64c251d21b098f1ca610a8829f
Aaron Amha <aaam7416@colorado.edu>
2025-11-07 17:58:50 -0700

    hello

--- MachineLearning/Basic.py
+++ MachineLearning/Basic.py
@@ -13,6 +13,21 @@ wine_reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv")
 wine_reviews.head() 
 # (129971, 14) # shape of the data (rows, columns)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # save filepath to variable for easier access
 melbourne_file_path = '../sMachineLearning/melb_data.csv'
 # read the data and store data in DataFrame titled melbourne_data
